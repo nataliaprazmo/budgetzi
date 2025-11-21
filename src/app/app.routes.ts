1f540a5ef4bce7c9ac47cf07d37f6df7354497b4
nataliaprazmo <prazmonatalia@gmail.com>
2025-11-21 13:02:21 +0100

    Added lazy import for dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
-import { Dashboard } from './components/dashboard/dashboard';
 import { AddTransaction } from './components/add-transaction/add-transaction';
 
 export const routes: Routes = [
-  { path: '', component: Dashboard },
+  {
+    path: '',
+    loadComponent: () => import('./components/dashboard/dashboard').then((m) => m.Dashboard),
+  },
   { path: 'add', component: AddTransaction },
   { path: 'edit/:id', component: AddTransaction },
 ];
